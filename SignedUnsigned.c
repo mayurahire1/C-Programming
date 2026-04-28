d1b581d1c3d90d81703aeef5c7e897b2d4731acb
Mayur Ahire <162018537+mayurahire1@users.noreply.github.com>
2026-04-28 22:17:46 +0530

    Add files via upload

---
+++ SignedUnsigned.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i = 11;             // Signed
+    int j = -11;            // Signed
+    unsigned int k = 11;    // unsigned
+    signed int a = 11;      // signed
+    signed int b = -11;     // signed
+
+    return 0;
+}
