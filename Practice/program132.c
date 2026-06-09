6067bb6b9bd736a38fb38f1fe8948ca65086eb51
Mayur Ahire <162018537+mayurahire1@users.noreply.github.com>
2026-06-09 09:15:56 +0530

    Add files via upload

---
+++ Practice/program132.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+// Time Complexity : O(N)
+
+bool LinearSearch(int Arr[], int iSize, int iNo)
+{
+    int iCnt = 0, iCount = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            iCount++;
+            break;
+        }
+    }
+
+    if(iCount == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0, iCnt = 0, iValue = 0; 
+    bool bRet = false;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &iLength);
+
+    Brr = (int *)malloc(sizeof(int) * iLength);
+
+    printf("Enter the elements: ");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d", &Brr[iCnt]);
+    }    
+
+    printf("Enter the element that you want to search: ");
+    scanf("%d", &iValue);
+
+    bRet = LinearSearch(Brr, iLength, iValue);
+
+    if(bRet == true)
+    {
+        printf("element is present");
+    }
+    else
+    {
+        printf("element is not present");
+    }
+
+    free(Brr);
+
+    return 0;
+}
---
+++ Practice/program136.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Time Complexity : O(N)
+
+int Minimum(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iMin = 0;
+
+    iMin = Arr[0];
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] < iMin)
+        {
+            iMin = Arr[iCnt];
+        }
+    }
+
+    return iMin;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0, iCnt = 0; 
+    int iRet = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &iLength);
+
+    Brr = (int *)malloc(sizeof(int) * iLength);
+
+    printf("Enter the elements: ");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d", &Brr[iCnt]);
+    }    
+
+    iRet = Minimum(Brr, iLength);
+
+    printf("Minimum element is : %d", iRet);
+
+    free(Brr);
+
+    return 0;
+}
